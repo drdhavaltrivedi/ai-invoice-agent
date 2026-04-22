ca986b7878749a15e085f38e72559322f6557ffe
drdhavaltrivedi <drdhavaltrivedi5@gmail.com>
2026-04-22 17:36:06 +0530

    chore: remove scratch scripts

--- check_invoice.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv("backend/.env")
-from app.core.supabase import get_supabase
-
-db = get_supabase()
-invoice_id = "141f98af-9bb6-476a-a86d-7dc7d44bbab1"
-res = db.table("invoices").select("*").eq("id", invoice_id).execute()
-print(res.data)
--- list_buckets.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv("backend/.env")
-from app.core.supabase import get_supabase
-
-db = get_supabase()
-res = db.storage.list_buckets()
-print(res)
